9f365970a6e09d6ed9e4ea02f724b0e524b6e2d5
DariaKelesh <75041274+DariaKelesh@users.noreply.github.com>
2022-08-08 16:53:15 +0300

    create MarkUp, generateContent update

--- src/index.js
+++ src/index.js
@@ -18,35 +18,58 @@ function showError(error) {
   countryList.innerHTML = '';
 }
 
-
-
-input.addEventListener("input", debounce(handleInput, DEBOUNCE_DELAY));
-
-const createLi = ([{name, capital, flag, population, languages}]) => {
-    `<li>
-    <img src="" alt="">
-    <p>${name.common}</p>
-    <p>${capital}</p>
-    <p>${population}</p>
-    <p>${languages}</p>
-    </li>
-`
-}
-
-const generateContent = (array) => array.reduce((acc, item) => acc + createLi(item) + "");
-
-const createMarkUp = (array) => {
-    const result = generateContent(array);
-    countryList.insertAdjacentHTML("beforeend", result)
-}
-
-
-function handleInput() {
-    const countryInput = input.value.trim();
-    fetchCountries(countryInput) 
-
+function createList(country) {
+    const markup = country
+      .map(({ name, flags }) => {
+        return `<li>
+      <img  src="${flags.svg}" 
+    width="30" alt="flag of ${name.common}"/>
+    <span >${name.official}</span></li>`;
+      })
+      .join('');
+    countryList.innerHTML = markup;
+  }
   
-    
-
-}
-
+  function createItem([{ name, capital, population, flags, languages }]) {
+    const markupItem = `
+    <div>
+        <img src="${flags.svg}" alt='${name.common}'>
+        <h1>${name.official}</h1>
+        <p>'Capital: ${capital}'</p>
+        <p>'Population: ${population}'</p>
+       
+        <p>'Languages: ${Object.values(languages).join(', ')}'</p>
+    </div>`;
+    countryInfo.innerHTML = markupItem;
+  }
+  
+  function handlerInput() {
+    let userInfo = inputSearch.value.trim();
+    if (userInfo === '') {
+      console.log('fill it');
+    } else {
+      fetchCountries(inputSearch.value.trim())
+        .then(country => {
+          countryInfo.innerHTML = '';
+          countryList.innerHTML = '';
+          if (country.length > 10) {
+            console.log(country, 'length > 10');
+            Notify.info(
+              'Too many matches found. Please enter a more specific name.'
+            );
+          } else if (country.length >= 2 && country.length <= 10) {
+            createList(country);
+            console.log(country, 'length 2, 10');
+          } else if (country.length === 1) {
+            Notify.success(
+              'You have found it!'
+            );
+            createItem(country);
+            console.log(country, 'length = 1');
+          }
+        })
+        .catch(showError);
+    }
+  }
+  
+  inputSearch.addEventListener('input', debounce(handlerInput, DEBOUNCE_DELAY));
